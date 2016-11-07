03d1d348730a4983eb1d6e1069e6ddbc2f36a39c
Nick Herlihy <njh1295@gmail.com>
2016-11-07 13:40:04 -0500

    Adding logout url

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from app.views import public
+from app.views import public, account
 
 urlpatterns = [
 	url(r'^$', public.home, name='home'),
+	url(r'^logout', account.log_out, name='logout'),
 ]
