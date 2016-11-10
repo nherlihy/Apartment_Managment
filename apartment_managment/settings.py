72120041ec8342752b7b7de50ebb32b8dd7e455a
Gagne <gagnej3@wit.edu>
2016-11-10 12:53:43 -0500

    useless

--- apartment_managment/settings.py
+++ apartment_managment/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'apartment_managment',
         'USER': 'root',
-        'PASSWORD': 'Csmmil115',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
