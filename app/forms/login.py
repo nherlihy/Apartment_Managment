02f204b9fdbbe74cf1b6988f7e02a0b34b48a7de
Nick Herlihy <njh1295@gmail.com>
2016-11-10 20:22:31 -0500

    Changing name to avoid conflicts with register form

--- app/forms/login.py
+++ app/forms/login.py
@@ -4,8 +4,8 @@ from django import forms
 from crispy_forms.helper import FormHelper
 
 class LoginForm(forms.Form):
-	username = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder' : 'Username'}))
-	password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+	login_username = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder' : 'Username'}))
+	login_password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
 
 	def __init__(self, *args, **kwargs):
 		super(LoginForm, self).__init__(*args, **kwargs)
