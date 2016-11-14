649682e2ac855a301b1268e3a0a74bc6f7afadf1
Nick Herlihy <njh1295@gmail.com>
2016-11-14 10:51:57 -0500

    Removing unused form for now

--- app/forms/member.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from app.models import Member
-from crispy_forms.helper import FormHelper
-
-class MemberForm(forms.ModelForm):
-    class Meta:
-        model = Member
-        fields = ('username', 'email', 'password')
