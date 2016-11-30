2d18dbef6238840d7853ed27d72eeb806d74984f
Nick Herlihy <njh1295@gmail.com>
2016-11-30 13:02:16 -0500

    Removing un-used routes

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -1,9 +1,1 @@
 from django.shortcuts import render
-from app.forms.expense import ExpenseForm
-from app.models.models import Expense
-
-
-def dashboard(request):
-    group_expenses = Expense.objects.order_by('id')
-    expense_form = ExpenseForm()
-    return render(request, 'dashboard/dashboard.html', {'expense_form': expense_form, 'group_expenses': group_expenses})
