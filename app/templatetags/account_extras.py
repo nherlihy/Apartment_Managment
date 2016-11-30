4190a4fadfad283b9880edf6809feafa3a2f6dbb
Gagne <gagnej3@wit.edu>
2016-11-30 16:27:13 -0500

    different colors for profile images

--- app/templatetags/account_extras.py
+++ app/templatetags/account_extras.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+from random import randint
 
 register = template.Library()
 
@@ -11,3 +12,13 @@ register = template.Library()
 def initials(name):
     return name[0]
 
+@register.filter(name='profile_image')
+def profile_image(value):
+    css_class = randint(1, 5)
+    return {
+        1: 'profile-1',
+        2: 'profile-2',
+        3: 'profile-3',
+        4: 'profile-4',
+        5: 'profile-5',
+    }[css_class]
