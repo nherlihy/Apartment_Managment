9346fa11f4d051fa992808d0f34a4bf07ef6efa9
Gagne <gagnej3@wit.edu>
2016-12-01 11:36:11 -0500

    removing password updating

--- app/forms/update_profile.py
+++ app/forms/update_profile.py
@@ -9,14 +9,14 @@ from crispy_forms.helper import FormHelper
 class UpdateProfile(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password')
+        fields = ('username', 'first_name', 'last_name', 'email') # , 'password')
 
     username = forms.CharField(required=False, label='Username', widget=forms.TextInput(attrs={'placeholder': 'Username'}))
     first_name = forms.CharField(required=False, label='First Name', widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
     last_name = forms.CharField(required=False, label='Last Name', widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
     email = forms.EmailField(required=False, label='Email', widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    password = forms.CharField(required=False, label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'New Password'}))
-    comfirm_password = forms.CharField(required=False, label='Confirm Password', widget=forms.PasswordInput(attrs={'placeholder': 'Confirm New Password'}))
+    # password = forms.CharField(required=False, label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'New Password'}))
+    # comfirm_password = forms.CharField(required=False, label='Confirm Password', widget=forms.PasswordInput(attrs={'placeholder': 'Confirm New Password'}))
 
     def __init__(self, *args, **kwargs):
         super(UpdateProfile, self).__init__(*args, **kwargs)
@@ -24,10 +24,10 @@ class UpdateProfile(forms.ModelForm):
 
     def clean(self):
         super(UpdateProfile, self).clean()
-        password = self.cleaned_data.get('password')
-        comfirm_password = self.cleaned_data.get('comfirm_password')
-
-        if password != comfirm_password:
-            raise forms.ValidationError({'password': ["Passwords dont match", ]})
+        # password = self.cleaned_data.get('password')
+        # comfirm_password = self.cleaned_data.get('comfirm_password')
+        #
+        # if password != comfirm_password:
+        #     raise forms.ValidationError({'password': ["Passwords dont match", ]})
 
         return self.cleaned_data
